f99f2e2104cfba368404bc87f0ce23ed8987ec73
Adrian <adrianrotama@beyondborders.jp>
2023-10-12 18:26:38 +0900

    ABOUT_SEKAI page

--- frontend/app/(main_app)/page.js
+++ frontend/app/(main_app)/page.js
@@ -1,3 +1,42 @@
 export default function Page() {
-  return <h1>Hello, a.js!</h1>;
+  return (
+    <section>
+      homepage
+      {/* <div>
+        <div className={`${styles.fv3LogoContainer}`}>
+          <div className={styles.fv3Logo}>
+            <img src='https://sekai-property-assets.s3.ap-northeast-1.amazonaws.com/images/TopFVFrame_2.png' />
+            <div className={`${styles.fv3LogoText} ${styles.text1}`}>
+              物件掲載数<br />
+              <span>約</span><span className={styles.fvMainText}>{aboutSekai.top_page_number_of_properties_listed}件</span>
+            </div>
+          </div>
+          <div className={styles.fv3Logo}>
+            <img src='https://sekai-property-assets.s3.ap-northeast-1.amazonaws.com/images/TopFVFrame_2.png' />
+            <div className={`${styles.fv3LogoText} ${styles.text2}`}>
+              総会員数<br />
+              <span>約</span><span className={styles.fvMainText}>{aboutSekai.top_page_number_of_customers}人</span>
+            </div>
+          </div>
+          <div className={styles.fv3Logo}>
+            <img src='https://sekai-property-assets.s3.ap-northeast-1.amazonaws.com/images/TopFVFrame_2.png' />
+            <div className={`${styles.fv3LogoText} ${styles.text3}`}>
+              物件契約数<br />
+              <span>約</span><span className={styles.fvMainText}>{aboutSekai.top_page_number_of_contracts}件</span>
+            </div>
+          </div>
+        </div>
+        <div className={styles.imageContainer}>
+          <Image
+            src="https://sekai-property-assets.s3.ap-northeast-1.amazonaws.com/images/about-sekai/2023_JOorganic_aboutUS_1.png"
+            width={996}
+            height={527}
+            alt="セカイプロパティ"
+          />
+        </div>
+      </div>
+    </div> */}
+    </section >
+  )
+
 }
