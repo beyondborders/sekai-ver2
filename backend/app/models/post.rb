17f590a0aeeef4b95bb8d53941fbfd214e890742
Deekshith Harish <harish@beyondborders.jp>
2023-12-13 19:57:09 +0530

    FEAT: IGNORE DELETED POSTS

--- backend/app/models/post.rb
+++ backend/app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ApplicationRecord
 
+  default_scope { where(deleted_at: nil) }
+
   has_many :post_images
   has_one :eyecatch_image, -> { eyecatch }, class_name: 'PostImage'
 
@@ -18,12 +20,12 @@ class Post < ApplicationRecord
 
   enum category: { tour: 'tour', knowhow: 'knowhow', news: 'news', interview: 'interview' }
 
-  def tag_related_posts(limit=10)
-    tag_post_ids = Post.published.order_desc.where(language_code: language_code).joins(:tags).where(tags: { id: tag_ids }).where.not(id: id).distinct.limit(limit).pluck(:id)
+  def tag_related_posts(limit=6)
+    tag_post_ids = Post.published.order_desc.joins(:tags).where(language_code: language_code, category: category, tags: { id: tag_ids }).where.not(id: id).distinct.limit(limit).pluck(:id)
     if tag_post_ids.length < limit
-      tag_post_ids.concat(Post.published.order_desc.where(language_code: language_code).where.not(id: tag_post_ids.push(id)).distinct.limit(limit-tag_post_ids.length).pluck(:id))
+      tag_post_ids.concat(Post.published.order_desc.where(language_code: language_code, category: category).where.not(id: tag_post_ids.push(id)).distinct.limit(limit-tag_post_ids.length).pluck(:id))
     end
-    Post.where(id: tag_post_ids)
+    Post.where(id: tag_post_ids).in_order_of(:id, tag_post_ids)
   end
 
   def self.ransackable_attributes(auth_object = nil)
